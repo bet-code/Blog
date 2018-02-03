d8b86781df6193e4cd4327cc9ae0b1e2b31b5269
Gilbert <gilbert.j5@outlook.com>
2018-02-03 14:55:02 -0500

    add pckg

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -6,9 +6,26 @@ import ReduxPromise from 'redux-promise';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import App from './components/app';
+import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise) (createStore);
 
+class Hello extends React.Component {
+  render() { return <div>Hello</div>}
+}
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye</div>}
+}
+
+
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router>
+        <div>
+          <Route path="/hello" component={Hello} />
+          <Route path="/goodbye" component={Goodbye} />
+        </div>
+      </Router>
+    </Provider>,
   document.getElementById('app'));
